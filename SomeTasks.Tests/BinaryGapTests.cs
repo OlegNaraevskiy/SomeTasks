128da036d2b70658631a1432d9c95511797d02c5
Oleg Naraevskiy <noa.oleg96@gmail.com>
2021-12-12 04:50:17 +0300

    + add XUnit test: SomeTasks.Tests + start BinaryGap task

---
+++ SomeTasks.Tests/BinaryGapTests.cs
@@ -0,0 +1,28 @@
+﻿/*===================================================================
+ * Copyright (c) 2021 Oleg Naraevskiy                   Date: 12.2021
+ * Version IDE: MS VS 2019
+ * Designed by: Oleg Naraevskiy / noa.oleg96@gmail.com      [12.2021]
+ *===================================================================*/
+
+using Xunit;
+ 
+namespace SomeTasks.Tests
+{
+	public class BinaryGapTests
+	{
+		[Fact]
+		public void IndexViewDataMessage()
+		{
+			// Arrange
+			BinaryGap binaryGap = new BinaryGap();
+			int number = 20;
+			int resultNumber = 1;
+
+			// Act
+			int result = binaryGap.Find(number);
+
+			// Assert
+			Assert.Equal(resultNumber, result);
+		}
+	}
+}
---
+++ SomeTasks/BinaryGap.cs
@@ -0,0 +1,44 @@
+﻿/*===================================================================
+ * Copyright (c) 2021 Oleg Naraevskiy                   Date: 12.2021
+ * Version IDE: MS VS 2019
+ * Designed by: Oleg Naraevskiy / noa.oleg96@gmail.com      [12.2021]
+ *===================================================================*/
+
+using System;
+
+namespace SomeTasks
+{
+	public class BinaryGap
+	{
+		public int Find(int number)
+		{
+			if (number <= 0)
+			{
+				return 0;
+			}
+
+			int result = 0;
+
+			string binNumber = Convert.ToString(number, 2);
+
+			char[] numArray = binNumber.ToCharArray();
+
+			int[] gapsCounter = new int[numArray.Length];
+			int zeroCounter = 0;
+
+			for (int i = 0; i < numArray.Length; i++)
+			{
+				if (numArray[i] == '1')
+				{
+
+				}
+				else if (numArray[i] == '0')
+				{
+					zeroCounter++
+				}
+			}
+
+			return result;
+		}
+	}
+}
